beba62e09e27d25633eb96721380d779be13a059
echo <echoe.g@yahoo.com>
2019-05-15 15:18:45 +0800

    VeeValidate

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,8 +3,10 @@ window.Vue = require('vue');
 window.axios = require('axios');
 
 import VueRouter from 'vue-router'
+import VeeValidate from 'vee-validate'
 
 Vue.use(VueRouter);
+Vue.use(VeeValidate);
 
 import router from './router'
 
--- resources/js/components/register/RegisterForm.vue
+++ resources/js/components/register/RegisterForm.vue
@@ -1,31 +1,52 @@
 <template>
     <form class="col s12" @submit.prevent="register">
-        <div class="input-field">
+        <div class="input-field" :class="{'has-error': errors.has('name')}">
             <input
                 type="text"
                 v-model="name"
+                v-validate="'required|min:6|max:24'"
                 class="validate"
                 placeholder="用户名"
                 name="name"
+                autocomplete="off"
                 required>
+            <span
+                v-show="errors.has('name')"
+                :class="{'has-error': errors.has('name')}">
+                {{ errors.first('name') }}
+            </span>
         </div>
-        <div class="input-field">
+        <div class="input-field" :class="{'has-error': errors.has('password')}">
             <input
                 type="password"
                 v-model="password"
+                v-validate="'required|min:6'"
                 placeholder="密码"
                 name="password"
                 class="validate"
+                autocomplete="off"
                 required>
+            <span
+                v-show="errors.has('password')"
+                :class="{'has-error': errors.has('password')}">
+                {{ errors.first('password') }}
+            </span>
         </div>
-        <div class="input-field">
+        <div class="input-field" :class="{'has-error': errors.has('password_confirmation')}">
             <input
                 type="password"
                 v-model="password_confirmation"
+                v-validate="{'required': 'true', 'is': password}"
                 placeholder="确认密码"
                 name="password_confirmation"
                 class="validate"
+                autocomplete="off"
                 required>
+            <span
+                v-show="errors.has('password_confirmation')"
+                :class="{'has-error': errors.has('password_confirmation')}">
+                {{ errors.first('password_confirmation') }}
+            </span>
         </div>
         <button type="submit" class="button-default">注册</button>
     </form>
@@ -56,6 +77,15 @@
     }
 </script>
 
-<style scoped>
-
+<style scoped lang="scss">
+    .has-error{
+        input{
+            border: #e3342f solid 1px !important;
+        }
+        color: #e3342f;
+        input[type=text].valid, input[type=text].valid{
+            border-bottom: 1px solid #e3342f;
+            box-shadow: 0 1px 0 0 #e3342f;
+        }
+    }
 </style>
